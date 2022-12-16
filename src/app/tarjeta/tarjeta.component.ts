5270cf7b85ebbd84b620274f50b9889579d91c2e
Alex Cruz <acruz@qualitrain.com.mx>
2022-12-16 14:35:32 -0600

    Ejemplo @ContentChild y @ContentChildren OK
    Método ngAfterContentInit
    Comentario sobre restricciones en uso de #vars de template

--- src/app/tarjeta/tarjeta.component.ts
+++ src/app/tarjeta/tarjeta.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input, ElementRef, ViewChild, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, AfterViewInit, ViewChildren, AfterContentInit,
+         QueryList, ContentChild, ContentChildren } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-tarjeta',
   templateUrl: './tarjeta.component.html',
   styleUrls: ['./tarjeta.component.css']
 })
-export class TarjetaComponent implements AfterViewInit{
+export class TarjetaComponent implements AfterViewInit, AfterContentInit{
+  ancho:number=18;
+
   @Input()
   debug:string="false";
 
@@ -30,6 +34,17 @@ export class TarjetaComponent implements AfterViewInit{
 
   urlsAccion:string="";
 
+  @ContentChild("titListaRelevante")
+  elemNgContent!:ElementRef;
+
+  titRelevante:string="";
+
+  // Inyectará los elementos DOM del ng-content con #Relevante del PRIMER NIVEL (No funciona en elementos anidados 2do o tercer nivel)
+  @ContentChildren('Relevante')
+  elemsRelevantes!:QueryList<ElementRef>;
+
+  textRelevante:string="";
+
   ngOnInit():void {
     if(this.debug==="true")
       this.debugOn =true;
@@ -38,20 +53,38 @@ export class TarjetaComponent implements AfterViewInit{
   }
   
   ngAfterViewInit(): void {
+    console.log("***************** ngAfterViewInit() *****************")
+    // ---------------- Mostrando valores de elemento inyectado @ViewChild ----------------
+
     /* 
     Se debe hacer así para evitar errores modificando una vista que ya estaba rendereada: Se forma el 
     cambio para hacerse en el siguiente ciclo de procesamiento de eventos
     */
     setTimeout(()=>this.urlImagen = this.elemImagen.nativeElement.src,0);
 
+    // ---------------- Mostrando valores de elementos inyectados @ViewChildren ----------------
     let urlsBtnsAccion:string[]=[];
     for (let elemI of this.botonesLink){
       urlsBtnsAccion.push(elemI.nativeElement.href);
     }
-    setTimeout(()=>this.urlsAccion = urlsBtnsAccion.join(", "),0);
-
-
-    console.log(urlsBtnsAccion.join(", "));
+    console.log("\nBotones:")
     console.log(this.botonesLink);
+    setTimeout(()=>this.urlsAccion = urlsBtnsAccion.join(", "),0);
+  }
+
+  ngAfterContentInit():void{
+    console.log("***************** ngAfterContentInit() *****************")
+    console.log(this.elemsRelevantes);
+    // ---------------- Mostrando valores de elemento inyectado @ContentChild ----------------
+    let div:HTMLDivElement = this.elemNgContent.nativeElement;
+    setTimeout(()=>this.titRelevante = div.innerText,0);
+    console.log(div);
+
+    // ---------------- Mostrando valores de elementos inyectados @ContentChildren ----------------
+    let textosRelevantes:string[]=[];
+    this.elemsRelevantes.forEach( elemI => textosRelevantes.push(elemI.nativeElement.innerText));
+    this.textRelevante = textosRelevantes.join(" / ");
+    console.log(this.elemsRelevantes);
+    console.log(textosRelevantes);
   }
 }
