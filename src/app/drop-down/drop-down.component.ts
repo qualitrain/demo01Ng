46caa7cb770c8178b2d98b0ba1aac4161c292324
Alex Cruz <acruz@qualitrain.com.mx>
2023-01-08 20:10:40 -0600

    corrección de drop-down.componente.ts

--- src/app/drop-down/drop-down.component.ts
+++ src/app/drop-down/drop-down.component.ts
@@ -19,13 +19,11 @@ export class DropDownComponent {
  nombreBoton:string="Indefinido";
  nomOpciones:string[]=["opcion 0", "opcion 1"];
  hRefOpciones:string[]=["#","#"]
- clicksPorOpcion:number[]=[];
 
  constructor(private provDropDown:IGestorOpcionesService) {
    this.nombreBoton = provDropDown.getNombreBoton();
    this.nomOpciones = provDropDown.getNombresOpciones();
    this.hRefOpciones = provDropDown.getLinks();
-   this.nomOpciones.forEach( elemI => this.clicksPorOpcion.push(0) ) ;
  }
  publicarEleccion(opcionElegida:string){
    this.provDropDown.publicar(opcionElegida);
