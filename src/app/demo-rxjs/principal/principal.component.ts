20e42db3cc9023e06778dc67b1b3aa087bee9096
Alex Cruz <acruz@qualitrain.com.mx>
2023-04-04 12:48:13 -0600

    routerLinkActive para estilizar opcion activa

--- src/app/demo-rxjs/principal/principal.component.ts
+++ src/app/demo-rxjs/principal/principal.component.ts
@@ -1,27 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
-
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
-export class PrincipalComponent implements OnInit{
-  opcion:boolean[]=[];
-  n:number=1;
-
-  constructor(private rutaActiva:ActivatedRoute){}
-
-  ngOnInit(): void {
-    this.rutaActiva.url.subscribe(fragsUrl => this.inicializarRutaActiva(fragsUrl));
-  }
-  inicializarRutaActiva(fragsUrl:UrlSegment[]){
-    console.log("Invocacion num " + this.n++);
-    console.log(fragsUrl);
-    this.opcion.forEach((val,idx,arr)=>arr[idx]=false); 
-  }
-  activarOpcion(nOpcion:number){
-    this.opcion.forEach((val,idx,arr)=>arr[idx]=false);
-    this.opcion[nOpcion]=true;
-  }
+export class PrincipalComponent {
 }
