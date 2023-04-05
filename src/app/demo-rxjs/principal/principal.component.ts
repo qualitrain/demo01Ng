6349b2d4b66d6b4f41ef22ab18da2a1abea97320
Alex Cruz <acruz@qualitrain.com.mx>
2023-04-04 21:59:33 -0600

    funcionalidades para monitoreo de ruteo
    Eventos
    Ruta Activa
    Rutas hijas

--- src/app/demo-rxjs/principal/principal.component.ts
+++ src/app/demo-rxjs/principal/principal.component.ts
@@ -1,8 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy,AfterViewInit  } from '@angular/core';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
+import { distinctUntilChanged, fromEvent, map, Subscription } from 'rxjs';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
-export class PrincipalComponent {
+export class PrincipalComponent implements AfterViewInit, OnDestroy{
+  suscrCambioRutaHija?:Subscription;
+
+  constructor(private rutaActiva:ActivatedRoute, 
+              private ruteador:Router ){
+              
+  }
+ 
+  ngAfterViewInit(): void {
+    this.ruteador.events.subscribe(evt => {
+      console.log("ruteador.events")
+      console.log(evt)
+     });
+
+    this.suscrCambioRutaHija = fromEvent(document.getElementById("ulMenu") as HTMLElement,"click")
+                               .pipe(map( ()=> this.rutaActiva.firstChild?.snapshot.url[0].path),
+                                     distinctUntilChanged())   
+                               .subscribe(rutaHija => this.monitorearRuta(rutaHija));
+
+    this.rutaActiva.url.subscribe(arrSegmentos => {
+      console.log("Suscripcion rutaActiva.url:")
+      console.log(arrSegmentos);
+    })
+  }
+   
+  monitorearRuta(rutaHija:string|undefined){
+    let rutaPadre= this.rutaActiva.snapshot.url[0].path
+    console.log("/" + rutaPadre + "/"+rutaHija);
+  }
+  ngOnDestroy(): void {
+    this.suscrCambioRutaHija?.unsubscribe();
+  }
 }
