650fcde2704b671d139846ae4fab584c74283ba7
Alex Cruz <acruz@qualitrain.com.mx>
2023-04-08 16:09:04 -0600

    actualizacion de @NgModule x componentes migrados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,24 +20,14 @@ import { NotificadorBajaComponent } from './notificador-baja/notificador-baja.co
 import { DropDownComponent } from './drop-down/drop-down.component';
 import { CajaDemoHostComponent } from './caja-demo-host/caja-demo-host.component';
 import { TituloComponent } from './titulo/titulo.component';
-import { TestRxjs01Component } from './demo-rxjs/test-rxjs01/test-rxjs01.component';
-import { TestRxjs02Component } from './demo-rxjs/test-rxjs02/test-rxjs02.component';
-import { TestRxjs03Component } from './demo-rxjs/test-rxjs03/test-rxjs03.component';
-import { TestRxjs04Component } from './demo-rxjs/test-rxjs04/test-rxjs04.component';
-import { TestRxjs05Component } from './demo-rxjs/test-rxjs05/test-rxjs05.component';
-import { TestRxjs06Component } from './demo-rxjs/test-rxjs06/test-rxjs06.component';
 import { TestRxjs07Component } from './test-rxjs07/test-rxjs07.component';
 import { TestRxjs08Component } from './test-rxjs08/test-rxjs08.component'
 import { Canvas01Component } from './canvas01/canvas01.component';
 import { TestRxjs09Component } from './test-rxjs09/test-rxjs09.component';
-import { TestRxjs10Component } from './demo-rxjs/test-rxjs10/test-rxjs10.component';
 import { TestRxjs11Component } from './test-rxjs11/test-rxjs11.component';
-import { TestRxjs12Component } from './demo-rxjs/test-rxjs12/test-rxjs12.component';
 import { TestRxjs13Component } from './test-rxjs13/test-rxjs13.component';
 import { TestRxjs12BComponent } from './test-rxjs12-b/test-rxjs12-b.component';
 import { TestRxjs14Component } from './test-rxjs14/test-rxjs14.component';
-import { TestRxjs15AjaxComponent } from './demo-rxjs/test-rxjs15-ajax/test-rxjs15-ajax.component';
-import { TestRxjs16SubjectComponent } from './demo-rxjs/test-rxjs16-subject/test-rxjs16-subject.component';
 import { TestRxjs17SchedulerComponent } from './test-rxjs17-scheduler/test-rxjs17-scheduler.component';
 import { DemoRxjsModule } from './demo-rxjs/demo-rxjs.module';
 import { PagNoExisteComponent } from './pag-no-existe/pag-no-existe.component';
@@ -59,24 +49,14 @@ import { PagNoExisteComponent } from './pag-no-existe/pag-no-existe.component';
     DropDownComponent,
     CajaDemoHostComponent,
     TituloComponent,
-    TestRxjs01Component,
-    TestRxjs02Component,
-    TestRxjs03Component,
-    TestRxjs04Component,
-    TestRxjs05Component,
-    TestRxjs06Component,
     TestRxjs07Component,
     TestRxjs08Component,
     Canvas01Component,
     TestRxjs09Component,
-    TestRxjs10Component,
     TestRxjs11Component,
-    TestRxjs12Component,
     TestRxjs13Component,
     TestRxjs12BComponent,
     TestRxjs14Component,
-    TestRxjs15AjaxComponent,
-    TestRxjs16SubjectComponent,
     TestRxjs17SchedulerComponent,
     PagNoExisteComponent
   ],
--- src/app/demo-rxjs/demo-rxjs.module.ts
+++ src/app/demo-rxjs/demo-rxjs.module.ts
@@ -6,11 +6,31 @@ import { PrincipalComponent } from './principal/principal.component';
 import { ListaDemosBasicosComponent } from './lista-demos-basicos/lista-demos-basicos.component';
 import { EditorComponent } from './editor/editor.component';
 import { FormsModule } from '@angular/forms';
+import { TestRxjs01Component } from './test-rxjs01/test-rxjs01.component';
+import { TestRxjs02Component } from './test-rxjs02/test-rxjs02.component';
+import { TestRxjs03Component } from './test-rxjs03/test-rxjs03.component';
+import { TestRxjs04Component } from './test-rxjs04/test-rxjs04.component';
+import { TestRxjs05Component } from './test-rxjs05/test-rxjs05.component';
+import { TestRxjs06Component } from './test-rxjs06/test-rxjs06.component';
+import { TestRxjs10Component } from './test-rxjs10/test-rxjs10.component';
+import { TestRxjs12Component } from './test-rxjs12/test-rxjs12.component';
+import { TestRxjs15AjaxComponent } from './test-rxjs15-ajax/test-rxjs15-ajax.component';
+import { TestRxjs16SubjectComponent } from './test-rxjs16-subject/test-rxjs16-subject.component';
 
 
 @NgModule({
   declarations: [
     PrincipalComponent,
+    TestRxjs01Component,
+    TestRxjs02Component,
+    TestRxjs03Component,
+    TestRxjs04Component,
+    TestRxjs05Component,
+    TestRxjs06Component,
+    TestRxjs10Component,
+    TestRxjs12Component,
+    TestRxjs15AjaxComponent,
+    TestRxjs16SubjectComponent,
     ListaDemosBasicosComponent,
     EditorComponent
   ],
@@ -18,6 +38,18 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     DemoRxjsRoutingModule
+  ],
+  exports:[
+    TestRxjs01Component,
+    TestRxjs02Component,
+    TestRxjs03Component,
+    TestRxjs04Component,
+    TestRxjs05Component,
+    TestRxjs06Component,
+    TestRxjs10Component,
+    TestRxjs12Component,
+    TestRxjs15AjaxComponent,
+    TestRxjs16SubjectComponent
   ]
 })
 export class DemoRxjsModule { }
